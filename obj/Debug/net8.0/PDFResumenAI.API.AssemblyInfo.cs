ecc417d4837cf795093de50c7b8f5c1c4b19cbe9
I202122563 (Carrasco Olivares <i202122563@cibertec.edu.pe>
2026-02-11 18:33:30 -0500

    Agregado Dockerfile para despliegue

--- obj/Debug/net8.0/PDFResumenAI.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/PDFResumenAI.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PDFResumenAI.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c1f30dbd775ef3aa50eb9d6489b42545533f07")]
 [assembly: System.Reflection.AssemblyProductAttribute("PDFResumenAI.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PDFResumenAI.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
